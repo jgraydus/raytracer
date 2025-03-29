486d56f30ed3e4cac08c8c53bd6f8052694906c4
josh <jgraydus@gmail.com>
2025-03-29 11:46:24 -0600

    config change

--- src/Main.hs
+++ src/Main.hs
@@ -12,14 +12,14 @@ s1 = Sphere (vec3 0.0 (-100.5) (-1.0)) 100
 
 main :: IO ()
 main = do
-  let height = 320
-      width = 400
+  let height = 640
+      width = 900
   let camera = Camera { imageInfo = ImageInfo { height, width }
                       , viewport = Viewport { height = 2.0
                                             , width = fromIntegral width / fromIntegral height * 2.0 }
                       , center = vec3 0.0 0.0 0.0
                       , focalLength = 1.0
-                      , samplesPerPixel = 10
+                      , samplesPerPixel = 20
                       , maximumBounces = 10
                       }
 
